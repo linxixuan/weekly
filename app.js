b67a4deaaef091062b5a48773993db21ecc7c600
linxixuan <zmx.morgan@gmail.com>
2015-01-21 10:10:16 +0800

    add style and database

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var koa = require('koa'),
     fs = require('fs'),
+    Weekly = require('./models/weekly'),
     app = koa();
 
 var staticServer = require('koa-static');
 
 var views = require('co-views');
 var render = views(__dirname + '/views', { ext: 'ejs' });
-var bodyParser = require('koa-body-parser');
+var bodyParser = require('koa-bodyparser');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/zmx');
+Weekly = mongoose.model('Weekly');
 
 // logger
 
@@ -26,12 +28,29 @@ app.use(bodyParser());
 
 app.use(function *(){
     if (this.method === 'POST') {
-        console.log(this.req.body);
+        var content = this.request.body.content,
+            mood = 1,
+            date = new Date();
+
+        var weekly = new Weekly({
+            content: content,
+            mood: mood,
+            date: date
+        });
+
+        // 在save的回调函数中设置body会报错，是不是两种机制的promise有冲突？
+        weekly.save();
+        this.body = JSON.stringify({
+            status: 1,
+            msg: '保存成功'
+        });
     } else {
-        if(/^\/list\W/.test(this.url)) {
+        if(/^\/edit/.test(this.url)) {
+            this.body = yield render('edit');
+        } else if (/^\/list/.test(this.url)) {
             this.body = yield render('list');
         } else {
-            this.body = '';
+            this.body = '哈哈哈，你找啥呢？';
         }
     }
 });
